1cb97333a8814a1566f034acabdc77dd693f278f
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-06-25 13:54:25 +0300

    add first commit

---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,19 @@
+package org.example.repository;
+
+import org.example.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface PostRepository {
+
+    public List<Post> all();
+
+    public Optional<Post> getById(long id);
+
+    public Post save(Post post);
+
+    public void removeById(long id);
+
+}
---
+++ src/main/java/org/example/repository/PostRepositoryImpl.java
@@ -0,0 +1,41 @@
+package org.example.repository;
+
+import org.springframework.stereotype.Repository;
+import org.example.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+    private final Map<Long, Post> mapOfPost = new ConcurrentHashMap<>();
+    private final AtomicLong lastId = new AtomicLong(0);
+    private final long startID = 0l;
+
+
+    public List<Post> all() {
+        return new ArrayList<>(mapOfPost.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(mapOfPost.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == startID) {
+            post.setId(lastId.incrementAndGet());
+        } else if (!mapOfPost.containsKey(post.getId())) {
+            post.setId(lastId.incrementAndGet());
+        }
+        mapOfPost.putIfAbsent(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        mapOfPost.remove(id);
+    }
+}
